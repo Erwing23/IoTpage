a9855191e9ee4ee33eadd369d4a54cb868d6d2e1
Erwing <erwingdelfines@hotmail.com>
2021-04-21 23:35:27 -0500

    mucho pendiente burger

--- js/burger.js
+++ js/burger.js
@@ -1,17 +1,5 @@
-(function() {
-
-    var hamburger = {
-      navToggle: document.querySelector('.nav-toggle'),
-      nav: document.querySelector('nav'),
-  
-      doToggle: function(e) {
-        e.preventDefault();
-        this.navToggle.classList.toggle('expanded');
-        this.nav.classList.toggle('expanded');
-      }
-    };
-  
-    hamburger.navToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
-    hamburger.nav.addEventListener('click', function(e) { hamburger.doToggle(e); });
-  
-  }());
+const hamburger = document.getElementById("hamburger");
+const navUl = document.getElementById("slide");
+hamburger.addEventListener("click",()=> {
+  navUl.classList.toggle("show");
+});
